5b60511b1137e3b61241c6dd9f336ebc3f9c18f1
Ne_Uchi <drmor107@gmail.com>
2026-03-27 20:50:10 +0500

    for now GameBoard can set and contain ships, check if coords is ok. For now new ship can be only in one coord (ship is 1x1 block even tho it has a length)

--- src/script.js
+++ src/script.js
@@ -14,12 +14,24 @@ class Ship {
   getHits() {
     return this.hits;
   }
+  getLength() {
+    return this.length;
+  }
 }
 
 class GameBoard {
   constructor() {
     this.height = 10;
     this.width = 10;
+    this.allShips = [];
+  }
+  getShips() {
+    return this.allShips;
+  }
+  setShip(x, y, ship) {
+    if (x > this.height || y + ship.getLength() > this.width) return false;
+    if (x < 0 || y < 0) return false;
+    this.allShips.push([x, y + ship.getLength()]);
   }
 }
 
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -23,4 +23,61 @@ describe('Ship Class', () => {
     expect(ship.isSunk()).toBe(true);
   });
 });
-describe('GameBoard Class', () => {});
+describe('GameBoard Class', () => {
+  let board;
+  beforeEach(() => {
+    board = new GameBoard();
+  });
+  it('testing an empty board', () => {
+    expect(board.getShips()).toEqual([]);
+  });
+  it('testing to set a proper ship coords 1', () => {
+    const ship = new Ship(5);
+    board.setShip(0, 0, ship);
+    expect(board.getShips()).toEqual([[0, 5]]);
+  });
+  it('testing to set a proper ship coords 2', () => {
+    const ship = new Ship(4);
+    board.setShip(2, 5, ship);
+    expect(board.getShips()).toEqual([[2, 9]]);
+  });
+  it('testing to set multiple ships with proper coords', () => {
+    const shipOne = new Ship(4);
+    const shipTwo = new Ship(2);
+    const shipThree = new Ship(5);
+    board.setShip(7, 3, shipOne);
+    board.setShip(2, 5, shipTwo);
+    board.setShip(0, 1, shipThree);
+    expect(board.getShips()).toEqual([
+      [7, 7],
+      [2, 7],
+      [0, 6],
+    ]);
+  });
+  it('testing to set wrong ship coords 1', () => {
+    const ship = new Ship(5);
+    board.setShip(11, 5, ship);
+    expect(board.setShip(11, 5, ship)).toBe(false);
+    expect(board.getShips()).toEqual([]);
+  });
+  it('testing to set wrong ship coords 2', () => {
+    const ship = new Ship(4);
+    board.setShip(8, 8, ship);
+    expect(board.setShip(8, 8, ship)).toBe(false);
+    expect(board.getShips()).toEqual([]);
+  });
+  it('testing to set multiple ships with wrong coords', () => {
+    const shipOne = new Ship(4);
+    const shipTwo = new Ship(2);
+    const shipThree = new Ship(5);
+    const shipFour = new Ship(2);
+    board.setShip(7, 3, shipOne);
+    board.setShip(12, 2, shipTwo);
+    board.setShip(10, 8, shipThree);
+    board.setShip(0, 8, shipFour);
+    expect(board.getShips()).toEqual([
+      [7, 7],
+      [0, 10],
+    ]);
+  });
+});
