649edeeca855aae9952d0fc4b2bd9079cc382b79
Ne_Uchi <drmor107@gmail.com>
2026-03-27 20:06:34 +0500

    created Ship class with functions, started GameBoard class

--- src/script.js
+++ src/script.js
@@ -1,1 +1,26 @@
-import "./style.css";
+//import "./style.css";
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+  }
+  hit() {
+    this.hits++;
+  }
+  isSunk() {
+    if (this.hits === this.length) return true;
+    else return false;
+  }
+  getHits() {
+    return this.hits;
+  }
+}
+
+class GameBoard {
+  constructor() {
+    this.height = 10;
+    this.width = 10;
+  }
+}
+
+module.exports = { Ship, GameBoard };
