85e5b574f4c606bd8b1081f4ec531293cd7d5819
Ne_Uchi <drmor107@gmail.com>
2026-03-28 21:18:55 +0500

    created 2 board, for now they do nothing

--- src/script.js
+++ src/script.js
@@ -1,15 +1,39 @@
 import './style.css';
 const { Ship, GameBoard, Player } = require('./AllClasses');
 const playBtn = document.querySelector('.playBtn');
-
+const p1container = document.querySelector('.p1Board');
+const p2container = document.querySelector('.p2Board');
+const p1Divs = [];
+const p2Divs = [];
+const player = new Player('p1');
+const computer = new Player('computer');
+const ship = new Ship(3);
+computer.board.setShip(0, 0, ship);
+player.board.setShip(5, 5, ship);
 playBtn.addEventListener('click', () => {
-  const player = new Player('p1');
-  const computer = new Player('computer');
-  const ship = new Ship(3);
-  computer.board.setShip(0, 0, ship);
-  player.board.setShip(5, 5, ship);
   console.log(player.board.getShips());
   console.log(computer.board.getShips());
-  //playBtn.style.display = 'none';
+  //   playBtn.style.display = 'none';
+  //   displayBoards();
 });
-function displayBoards() {}
+function displayBoards(container, arr) {
+  container.style.gridTemplateColumns = `repeat(10, 1fr)`;
+  container.style.gridTemplateRows = `repeat(10, 1fr)`;
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const cellDiv = document.createElement('div');
+      cellDiv.classList.add('block');
+      cellDiv.dataset.x = i;
+      cellDiv.dataset.y = j;
+      container.appendChild(cellDiv);
+      arr.push(cellDiv);
+    }
+  }
+}
+function attack(arr) {
+  arr.forEach((div) => {
+    div.addEventListener('click', () => {});
+  });
+}
+displayBoards(p1container, p1Divs);
+displayBoards(p2container, p2Divs);
