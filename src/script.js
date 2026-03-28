68215a7512b1c18397c2f9433f4b9cc3d5616abb
Ne_Uchi <drmor107@gmail.com>
2026-03-28 23:40:45 +0500

    now boards can display ships

--- src/script.js
+++ src/script.js
@@ -16,7 +16,7 @@ playBtn.addEventListener('click', () => {
   //   playBtn.style.display = 'none';
   //   displayBoards();
 });
-function displayBoards(container, arr) {
+const displayBoards = (container, arr, target) => {
   container.style.gridTemplateColumns = `repeat(10, 1fr)`;
   container.style.gridTemplateRows = `repeat(10, 1fr)`;
   for (let i = 0; i < 10; i++) {
@@ -29,11 +29,29 @@ function displayBoards(container, arr) {
       arr.push(cellDiv);
     }
   }
-}
+  displayShips(target, arr);
+};
+const displayShips = (target, arr) => {
+  for (let i = 0; i < target.board.allShips.length; i++) {
+    for (let x = target.board.allShips[i].startX; x <= target.board.allShips[i].endX; x++) {
+      for (let y = target.board.allShips[i].startY; y <= target.board.allShips[i].endY; y++) {
+        for (let j = 0; j < arr.length; j++) {
+          if (arr[j].dataset.x == x && arr[j].dataset.y == y) {
+            arr[j].style.backgroundColor = 'darkblue';
+          }
+        }
+      }
+    }
+  }
+};
+const displayShots = () => {};
 function attack(arr) {
   arr.forEach((div) => {
-    div.addEventListener('click', () => {});
+    div.addEventListener('click', (e) => {
+      console.log(e.target.dataset);
+    });
   });
 }
-displayBoards(p1container, p1Divs);
-displayBoards(p2container, p2Divs);
+attack(p1Divs);
+displayBoards(p1container, p1Divs, player);
+displayBoards(p2container, p2Divs, computer);
