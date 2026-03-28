037296bcec2d829fadcb346b0cf5de08c1557ca0
Ne_Uchi <drmor107@gmail.com>
2026-03-28 20:01:19 +0500

    stated doind UI, added BG and playBtn, for now it only create player and computer with one ship on board

--- src/script.js
+++ src/script.js
@@ -1,2 +1,15 @@
 import './style.css';
 const { Ship, GameBoard, Player } = require('./AllClasses');
+const playBtn = document.querySelector('.playBtn');
+
+playBtn.addEventListener('click', () => {
+  const player = new Player('p1');
+  const computer = new Player('computer');
+  const ship = new Ship(3);
+  computer.board.setShip(0, 0, ship);
+  player.board.setShip(5, 5, ship);
+  console.log(player.board.getShips());
+  console.log(computer.board.getShips());
+  //playBtn.style.display = 'none';
+});
+function displayBoards() {}
